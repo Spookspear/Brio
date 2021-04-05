4524c887602f7e36c20683b752d5575002ec594e
Spookspear <Grant_Bishop@Hotmail.com>
2021-04-05 12:15:49 +0100

    some mods

--- input.h
+++ input.h
@@ -1,151 +1,165 @@
 !                       Input.h
-! Use this file to get all the inputs from the PRM file
+! Use this file to get all the inputs From the PRM file
 !
 
 !#include 'input.{targetlanguage}'
 #Include 'multschm.h'
 !#Include '\\eugras135\c\sqr6\reports\multschm.h'
 
-begin-procedure get-input
+Begin-Procedure get-input
 
-input $where   {whereprompt}
+    Input $Where   {Whereprompt}
 
-if {p1type} = 'DATE'
-input $P1      {p1prompt} type=DATE format=DATE Status=#p1_status
-else
-input $P1      {p1prompt}
-end-if
+    If {p1type} = 'DATE'
+        Input $P1      {p1prompt} type=DATE format=DATE Status=#p1_status
+    Else
+        Input $P1      {p1prompt}
+    End-If
 
-if {p2type} = 'DATE'
-input $P2      {p2prompt} type=DATE format=DATE Status=#p2_status
-else
-input $P2      {p2prompt}
-end-if
+    If {p2type} = 'DATE'
+        Input $P2      {p2prompt} type=DATE format=DATE Status=#p2_status
+    Else
+        Input $P2      {p2prompt}
+    End-If
 
-if {p3type} = 'DATE'
-input $P3      {p3prompt} type=DATE format=DATE Status=#p3_status
-else
-input $P3      {p3prompt}
-end-if
+    If {p3type} = 'DATE'
+        Input $P3      {p3prompt} type=DATE format=DATE Status=#p3_status
+    Else
+        Input $P3      {p3prompt}
+    End-If
 
-if {p4type} = 'DATE'
-input $P4      {p4prompt} type=DATE format=DATE Status=#p4_status
-else
-input $P4      {p4prompt}
-end-if
+    If {p4type} = 'DATE'
+        Input $P4      {p4prompt} type=DATE format=DATE Status=#p4_status
+    Else
+        Input $P4      {p4prompt}
+    End-If
 
-input $schema  {schemaprompt}
+    Input $schema  {schemaprompt}
 
-	! modify the where if needed
-	do GetSchema( $schema, $where )
+    ! modify the Where If needed
+    Do GetSchema( $schema, $Where )
+    Do set-security ( $schema )
 
-    do set-security ( $schema )
-
-end-procedure
-
-
-begin-procedure set-security ( $schema )
-
-begin-sql
-begin
-[$schema]BPSECURITY_CTX.SET_SECURITY( $_username ,'SQRW');;
-DBMS_APPLICATION_INFO.SET_CLIENT_INFO( $_username||'@SQRW' );;
-end;;
-end-sql
-
-end-procedure
+End-Procedure
 
 
+Begin-Procedure set-security ( $schema )
 
+    Begin-Sql
+        Begin
+            BPSECURITY_CTX.SET_SECURITY( $_username ,'SQRW');;
+            DBMS_APPLICATION_INFO.SET_CLIENT_INFO( $_username||'@SQRW' );;
+        End;;
+    End-Sql
+
+End-Procedure
 
 
 !--------------- Check user is in a valid group. If no entry in maxvars fro report assume user is ok -----
-Begin-procedure user_check
-let $groups=''
-let $users=''
-Begin-select
-GROUPS          &groups
-USERS           &users
- move &groups to $groups
- move &users to  $users
-from [$schema]BPREPRES where REPORT=$report_name
-End-select
+Begin-Procedure user_check
+    Let $groups=''
+    Let $users=''
+
+    Begin-Select
+        GROUPS          &groups
+        USERS           &users
+        Move &groups to $groups
+        Move &users to  $users
+        From BPREPRES Where REPORT=$report_name
+    End-Select
 
 
-if $groups<>''
-! There is an entry in REPREST  for report and some groups are valid
-  let $userok='N'                                    ! assume user is not valid
-  let #totlength = length($groups)                   ! set the total length of all groups ($groups)
-  let #char=0                                        ! position in $groups
-  let #start=1                                       ! starting position of group
-  while 1 = 1
-    let #char = #char + 1                            ! move pointer 1 character to right in $groups
-    if #char > #totlength                            ! end of groups
-      break
-    end-if
-    if substr($groups,#char,1)=','                   ! found a comma
-      let #length = #char-#start                     ! work out length of group code
-      let $groupcode=substr($groups,#start,#length)  ! extract group code from $groups
-! ----- find if user and group are set up in the MAXUSERGROUPS table ----------
- uppercase $username
- uppercase $groupcode
+    If $groups<>''
 
-begin-select
-usrname   &usr1
-  let $userok='Y'                                    ! set the flag to indicate user is valid
-from [$schema]maxusergroups
-where usrname = $username
-and grpname = $groupcode
-end-select
-        let #start = #char + 1                       ! position the new start of group code to 1 past the comma
-    end-if
-  end-while
-! Need to process the last group code which is after the last comma in $groups
-  let #length = #char-#start                         ! work out length of group code
-  let $groupcode=substr($groups,#start,#length)      ! extract group code from $groups
+        ! There is an entry in REPREST  for report And some groups are valid
+        Let $userok='N'                                         ! assume user is not valid
+        Let #totlength = Length($groups)                        ! Set the total length of all groups ($groups)
+        Let #char=0                                             ! Position in $groups
+        Let #start=1                                            ! starting Position of group
 
-! ----- find if user and group are set up in the MAXUSERGROUPS table ----------
-uppercase $username
-uppercase $groupcode
-begin-select
-usrname  &usr2
- let $userok='Y'                                     ! set the flag to indicate user is valid
-from [$schema]maxusergroups
-where usrname = $username
-and grpname = $groupcode
-end-select
-end-if
+        While 1 = 1
+            Let #char = #char + 1                               ! Move pointer 1 character to right in $groups
+            If #char > #totlength                               ! End of groups
+                Break
+            End-If
 
+            If SubStr($groups,#char,1)=','                      ! found a comma
+                Let #length = #char-#start                      ! work out length of group code
+                Let $groupcode=SubStr($groups,#start,#length)   ! extract group code From $groups
 
-! ---- User is not part of a valid group, check for them being a valid user -------
-if $userok<>'Y' and $users<>''
-! There is an entry in REPREST  for report and some users are valid
-  let $userok='N'                                    ! assume user is not valid
-  let #totlength = length($users)                   ! set the total length of all users ($users)
-  let #char=0                                        ! position in $users
-  let #start=1                                       ! starting position of user
-  while 1 = 1
-    let #char = #char + 1                            ! move pointer 1 character to right in $users
-    if #char > #totlength                            ! end of users
-      break
-    end-if
-    if substr($users,#char,1)=','                   ! found a comma
-      let #length = #char-#start                     ! work out length of user code
-      let $usercode=substr($users,#start,#length)  ! extract user code from $users
-! ----- find if $usercode is the same as the user extracted ----------
-      if upper($username)=upper($usercode)
-       let $userok='Y'                                    ! set the flag to indicate user is valid
-      end-if
-      let #start = #char + 1                       ! position the new start of user code to 1 past the comma
-    end-if
-  end-while
-! Need to process the last user code which is after the last comma in $users
-  let #length = #char-#start                         ! work out length of user code
-  let $usercode=substr($users,#start,#length)      ! extract user code from $users
-! ----- find if $usercode is the same as the user extracted ----------
-  if upper($username)=upper($usercode)
-   let $userok='Y'                                    ! set the flag to indicate user is valid
-  end-if
-end-if
-End-procedure user_check
+                ! ----- find If user And group are Set up in the MAXUSERGROUPS table ----------
+                 UpperCase $username
+                 UpperCase $groupcode
+
+                Begin-Select
+                    USRNAME   &usr1
+                    Let $userok='Y'                             ! Set the flag to indicate user is valid
+                    From MAXUSERGROUPS
+                    Where USRNAME = $username
+                    And GRPNAME = $groupcode
+                End-Select
+
+                Let #start = #char + 1                          ! Position the new start of group code to 1 past the comma
+
+            End-If
+
+        End-While
+
+        ! Need to process the last group code which is after the last comma in $groups
+        Let #length = #char-#start                              ! work out length of group code
+        Let $groupcode=SubStr($groups,#start,#length)           ! extract group code From $groups
+
+        ! ----- find If user And group are Set up in the MAXUSERGROUPS table ----------
+
+        UpperCase $username
+        UpperCase $groupcode
+
+        Begin-Select
+            usrname  &usr2
+            Let $userok='Y'                                     ! Set the flag to indicate user is valid
+            From MAXUSERGROUPS
+            Where USRNAME = $username
+            And GRPNAME = $groupcode
+        End-Select
+
+    End-If
+
+    ! ---- User is not part of a valid group, check for them being a valid user -------
+    If $userok<>'Y' And $users<>''
+        ! There is an entry in REPREST  for report And some users are valid
+        Let $userok='N'                                         ! assume user is not valid
+        Let #totlength = Length($users)                         ! Set the total length of all users ($users)
+        Let #char=0                                             ! Position in $users
+        Let #start=1                                            ! starting Position of user
+
+        While 1 = 1
+        Let #char = #char + 1                                   ! Move pointer 1 character to right in $users
+            If #char > #totlength                               ! End of users
+                Break
+            End-If
+
+            If SubStr($users,#char,1)=','                       ! found a comma
+                Let #length = #char-#start                      ! work out length of user code
+                Let $usercode=SubStr($users,#start,#length)     ! extract user code From $users
+
+                ! ----- find If $usercode is the same as the user extracted ----------
+                If Upper($username)=Upper($usercode)
+                    Let $userok='Y'                             ! Set the flag to indicate user is valid
+                End-If
+
+                Let #start = #char + 1                          ! Position the new start of user code to 1 past the comma
+            End-If
+        End-While
+
+        ! Need to process the last user code which is after the last comma in $users
+        Let #length = #char-#start                              ! work out length of user code
+        Let $usercode=SubStr($users,#start,#length)             ! extract user code From $users
+
+        ! ----- find If $usercode is the same as the user extracted ----------
+        If Upper($username)=Upper($usercode)
+            Let $userok='Y'                                     ! Set the flag to indicate user is valid
+        End-If
+    End-If
+
+End-Procedure user_check
 
